ac5fe0349003e90ed6e5bf0312579329d5bc2a8e
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-10-08 16:06:52 +0300

    Image model defined successfully

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ import datetime as dt
 
 # location model
 class Location(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=50, unique=True)
 
     # save location to database
     def save_location(self):
@@ -18,6 +18,86 @@ class Location(models.Model):
 
      # delete location from database
     def delete_location(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
+
+
+# category model
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    # save category to database
+    def save_category(self):
+        self.save()
+
+    # update category
+    def update_category(self, name):
+        self.name = name
+        self.save()
+
+    # delete category from database
+    def delete_category(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
+
+
+# Image model
+class Image(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # save image to database
+    def save_image(self):
+        self.save()
+
+    # update image
+    def update_image(self, name, description, location, category):
+        self.name = name
+        self.description = description
+        self.location = location
+        self.category = category
+        self.save()
+
+    # get all images
+    @classmethod
+    def get_all_images(cls):
+        images = Image.objects.all()
+        return images
+
+    # get image by id
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = Image.objects.get(id=id)
+        return image
+
+    # get images by location
+    @classmethod
+    def get_images_by_location(cls, location):
+        images = Image.objects.filter(location__name=location)
+        return images
+
+    # get images by category
+    @classmethod
+    def get_images_by_category(cls, category):
+        images = Image.objects.filter(category__name=category)
+        return images
+
+    # get images by date
+    # @classmethod
+    # def get_images_by_date(cls, date):
+    #     images = Image.objects.filter(created_at__date=date)
+    #     return images
+
+    # delete image from database
+    def delete_image(self):
         self.delete()
 
     def __str__(self):
