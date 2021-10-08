c5c2a2aac1a92b6968a14525d535bdfe95b95736
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-10-08 15:59:37 +0300

    location model defined successfully

--- app/models.py
+++ app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+import datetime as dt
 # Create your models here.
+
+
+# location model
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+
+    # save location to database
+    def save_location(self):
+        self.save()
+
+    # update location
+    def update_location(self, name):
+        self.name = name
+        self.save()
+
+     # delete location from database
+    def delete_location(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
