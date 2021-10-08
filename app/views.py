206cebc2986b0fdca929ffa81d815193e435d8ee
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-10-08 17:24:17 +0300

    homepage wed ui done

--- app/views.py
+++ app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from .models import Image, Category, Location
 # Create your views here.
+
+
 def index(request):
-    return render(request, 'index.html')
+    images = Image.objects.all()
+    locations = Location.objects.all()
+    categories = Category.objects.all()
+    title = 'Homepage'
+    return render(request, 'index.html', {'images': images, 'locations': locations, 'categories': categories, 'title': title})
