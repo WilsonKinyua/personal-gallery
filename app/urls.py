cda21278a98a11ab551e097aab7ccecc506c9d2a
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-10-08 18:56:00 +0300

    searching of photos by image location done

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('search/', views.search, name='search'),
+
 ]
 
 if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
--- app/views.py
+++ app/views.py
@@ -9,3 +9,19 @@ def index(request):
     categories = Category.objects.all()
     title = 'Homepage'
     return render(request, 'index.html', {'images': images, 'locations': locations, 'categories': categories, 'title': title})
+
+
+def search(request):
+    if 'category' in request.GET and request.GET["category"]:
+        # change the search to be in lowercase
+        search_term = request.GET.get("category").lower()
+        searched_images = Image.filter_by_category(search_term)
+        message = f"{search_term}"
+        locations = Location.objects.all()
+
+        return render(request, 'search.html', {"message": message, "images": searched_images, 'locations': locations})
+
+    else:
+        locations = Location.objects.all()
+        message = "You haven't searched for any term"
+        return render(request, 'search.html', {"message": message, 'locations': locations})
